04b6dea2dae4848fe7597a05486c52238cea8ef9
Hoormazd <hoormazd@pirayeshfar.ch>
2022-12-10 18:43:20 +0100

    sudoku model

---
+++ model/sudoku.js
@@ -0,0 +1,11 @@
+const fs = require('fs-extra');
+const path = require('path');
+
+function newGame(){
+    //TODO Set up the board and start a new game
+    //return the board
+}
+
+module.exports = {
+    newGame
+}
--- routes/sudoku.js
+++ routes/sudoku.js
@@ -3,10 +3,14 @@ const router = express.Router();
 module.exports = router;
 
 let {model} = require("../model");
+const sudokumodel = require('../model/sudoku');
 
 router.post("/sudoku/new_game", function(req,res) {
     //TODO: Login verification
     //res.render("sudoku", {PASS PLAYER AND DIFFICULTY INFORMATION AFTER LOGIN} );
+
+    //TODO: Call sudokumodel (model/sudoku.js) to set up the game
+
     
     res.render("sudoku");
 });
