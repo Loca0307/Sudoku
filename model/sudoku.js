c94a7f01bd5f95ad9a78de6235eb39f0c0e2978f
Luca Beltrami <loca2gaming@gmail.com>
2022-12-10 20:55:43 +0100

    added database insertion

--- model/sudoku.js
+++ model/sudoku.js
@@ -8,16 +8,10 @@ let gridValues = []; //1D array representing board
 let sudoku = []; //2D array representing board
 
 
-
-
-
-
-
-function newGame(){
+function newGame() {
     //TODO Set up the board and start a new game
     //return the board
 }
-
 
 
 function arrayto2D() {
