43db171bf3a7c16500ea249a3a2a2c888964e053
Hoormazd <hoormazd@pirayeshfar.ch>
2022-12-10 19:09:50 +0100

    Routing

--- routes/sudoku.js
+++ routes/sudoku.js
@@ -6,7 +6,10 @@ let {model} = require("../model");
 const sudokumodel = require('../model/sudoku');
 
 router.post("/sudoku/new_game", function(req,res) {
+    let data = {username: req.body.username, diff: req.body.diff};
+
     //TODO: Login verification
+    //get information from the 
     //res.render("sudoku", {PASS PLAYER AND DIFFICULTY INFORMATION AFTER LOGIN} );
 
     //TODO: Call sudokumodel (model/sudoku.js) to set up the game
