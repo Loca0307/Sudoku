54eb70bbc7a217d383477c5d8bd665d99d83d7f4
Luca Beltrami <loca2gaming@gmail.com>
2022-12-14 23:53:46 +0100

    .

--- routes/high_scores.js
+++ routes/high_scores.js
@@ -4,59 +4,33 @@ module.exports = router;
 
 let {model} = require("../model");
 
-<<<<<<< Updated upstream
-router.post("/high_scores/solo", function (req, res) {
-    var highscore = {
-        username : req.body.username,
-        game : req.body.game,
-        boardObjHistory : req.body.boardObjHistory,
-        totalMoves : req.body.totalMoves
-    }
-=======
 
-router.post("/sudoku", (req, res) => {
+router.post("/", (req, res) => {
     let obj = {
         player: req.body.player,
         score: Number.parseInt(req.body.score)
     }
-    model.high_scores.insertOne(obj).then(high_scores => {
+    model.high_scores.inserOne(obj).then(high_scores => {
         res.format({'text/html' : () => {
-            res.redirect("/high_scores");
+            res.redirect("/index.html");
         },
         'application/json' : () => {
             res.status(201).json(obj);
         }
     })
     })
-}) 
+})
 
-/* router.post("/sudoku", function(req,res) {
-    let pagedata = {username: req.body.username, diff: req.body.diff, score: 0};
+router.get(["/", "index", "/index.html"], (req, res) => {
 
     model.high_scores.find({}).toArray().then((high_scores)=> {
         res.format({
-            'text/html': function () {
-                res.render("high_scores" , {
-                high_scores: high_scores
-            });
-        },
-            'application/json': function () {
-                res.status(201).json(sudoku);
-            }   
-        }); 
-    });
-    
-});
- */
-router.get(["/", "index", "/index.html"], (req, res) => {
->>>>>>> Stashed changes
-
-    model.high_scores.insertOne(highscore).then(ans => {
-                res.format({
-                    'application/json': function () {
-                        console.log(ans);
-                        res.status("201").json(ans);
-                    }
-        });
+            'text/html': () => {
+                res.render("", high_scores);
+                },
+            'application/json' : () => {
+                res.json(high_scores);
+            }
+        })
     });
 });
