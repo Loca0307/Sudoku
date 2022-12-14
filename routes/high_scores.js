a22852b0042edf6f4468e8fedb90075e37d3ffa3
Hoormazd <hoormazd@pirayeshfar.ch>
2022-12-14 22:00:51 +0100

    routes fix

--- routes/high_scores.js
+++ routes/high_scores.js
@@ -4,7 +4,6 @@ module.exports = router;
 
 let {model} = require("../model");
 
-<<<<<<< Updated upstream
 router.post("/high_scores/solo", function (req, res) {
     var highscore = {
         username : req.body.username,
@@ -12,44 +11,6 @@ router.post("/high_scores/solo", function (req, res) {
         boardObjHistory : req.body.boardObjHistory,
         totalMoves : req.body.totalMoves
     }
-=======
-
-router.post("/sudoku", (req, res) => {
-    let obj = {
-        player: req.body.player,
-        score: Number.parseInt(req.body.score)
-    }
-    model.high_scores.insertOne(obj).then(high_scores => {
-        res.format({'text/html' : () => {
-            res.redirect("/high_scores");
-        },
-        'application/json' : () => {
-            res.status(201).json(obj);
-        }
-    })
-    })
-}) 
-
-/* router.post("/sudoku", function(req,res) {
-    let pagedata = {username: req.body.username, diff: req.body.diff, score: 0};
-
-    model.high_scores.find({}).toArray().then((high_scores)=> {
-        res.format({
-            'text/html': function () {
-                res.render("high_scores" , {
-                high_scores: high_scores
-            });
-        },
-            'application/json': function () {
-                res.status(201).json(sudoku);
-            }   
-        }); 
-    });
-    
-});
- */
-router.get(["/", "index", "/index.html"], (req, res) => {
->>>>>>> Stashed changes
 
     model.high_scores.insertOne(highscore).then(ans => {
                 res.format({
