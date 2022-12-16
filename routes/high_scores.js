bbeaa5394724c7bfcd61b5560653a3bdaff2f4ec
Hoormazd <hoormazd@pirayeshfar.ch>
2022-12-16 10:42:24 +0100

    multiple fixes

--- routes/high_scores.js
+++ routes/high_scores.js
@@ -14,7 +14,6 @@ router.post("/high_scores/solo", function (req, res) {
         totalMoves : req.body.totalMoves,
         score : req.body.score
     }
-    console.log();
 
     model.high_scores.insertOne(highscore).then(ans => {
                 res.format({
